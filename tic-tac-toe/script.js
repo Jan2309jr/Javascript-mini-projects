8ed5fc820ea61bf05192625d88878f8ea5abbbd9
jan2309jr <jan2309jr@gmail.com>
2025-06-30 19:34:11 +0530

    adding changes

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,30 +1,55 @@
 const win=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 let xChance=true;
-let winnerX=true;
-let winnerY=true;
+let winnerX=false;
+let winnerO=false;
 let boxes=document.querySelectorAll(".btn");
 let reset=document.querySelector(".rbtn-bottom");
 let newGame=document.querySelector(".R-btn");
 let wText=document.querySelector(".wText");
+
+const disableBoxes=()=>{
+    for(let el of boxes){
+        el.disabled=true;
+    }
+};
+const enableBoxes=()=>{
+    xChance=true;
+    wText.classList.add("hide");
+    newGame.classList.add("hide");
+    for(let el of boxes){
+        el.innerText="";
+        el.disabled=false;
+    }
+};
+const resetBtn=()=>{
+    enableBoxes();
+};
+
 const checkWinner=()=>{
-    for(pattern of win){
-        console.log(boxes[pattern[0]].innerText,boxes[pattern[1]].innerText,boxes[pattern[2]].innerText);
-        let pos1=boxes[pattern[0]].innerText;
-        let pos2=boxes[pattern[1]].innerText;
-        let pos3=boxes[pattern[2]].innerText;
-        if(pos1==pos2 && pos2==pos3){
-            if(pos1==X){
-                winnerX=true;
-                return true;
-            }else{
-                winnerY=true;
+    for(let el of win){
+        let pos0=boxes[el[0]].innerText;
+        let pos1=boxes[el[1]].innerText;
+        let pos2=boxes[el[2]].innerText;
+        if(pos0!="" && pos1!="" && pos2!=""){
+            if(pos0==pos1 && pos1==pos2){
+                if(pos0=="X"){
+                    winnerX=true;
+                    wText.innerText="X won!";
+                    wText.classList.remove("hide");
+                    newGame.classList.remove("hide");
+                }else{
+                    winnerO=true;
+                    wText.innerText="O won!";
+                    wText.classList.remove("hide");
+                    newGame.classList.remove("hide");
+                }
                 return true;
             }
-        }else{
-            return false;
         }
     }
-}
+    return false;
+};
+
 boxes.forEach(el => {
     el.addEventListener("click",()=>{
         if(xChance){
@@ -37,11 +62,10 @@ boxes.forEach(el => {
         el.disabled=true;
         checkWinner();
         if(checkWinner()){
-            if(winnerX){
-                wText.innerText="X Won!";
-            }else{
-                wText.innerText="Y Won!";
-            }
+            disableBoxes();
         }
     });
 });
+
+reset.addEventListener("click",resetBtn);
+newGame.addEventListener("click",resetBtn);
