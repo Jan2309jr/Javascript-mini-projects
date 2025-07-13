c6a5b9e66f45c3789044a6b7e5a2b0a497c432aa
jan2309jr <jan2309jr@gmail.com>
2025-07-13 16:02:36 +0530

    updated script.js

--- rock-paper-scissor/script.js
+++ rock-paper-scissor/script.js
@@ -7,44 +7,36 @@ let compS=document.querySelector(".compScore");
 let reset=document.querySelector(".btn")
 let yourScore=0;
 let compScore=0;
-const check=()=>{
-    const elements=["r","p","s"];
+const elements=["r","p","s"];
+const getCompSchoice=()=>{
     choice=Math.floor(Math.random() * elements.length);
-    rock.addEventListener("click",()=>{
-        if(elements[choice]=="p"){
-            stat.innerText="Computer Won!";
-            compScore+=1;
-        }else if(elements[choice]=="s"){
-            stat.innerText="You Won!";
-            yourScore+=1;
-        }else{
-            stat.innerText="Draw";
-        }
-    });
-    paper.addEventListener("click",()=>{
-        if(elements[choice]=="s"){
-            stat.innerText="Computer Won!";
-            compScore+=1;
-        }else if(elements[choice]=="r"){
-            stat.innerText="You Won!";
-            yourScore+=1;
-        }else{
-            stat.innerText="Draw";
-        }
-    });
-    scissor.addEventListener("click",()=>{
-        if(elements[choice]=="p"){
-            stat.innerText="Computer Won!";
-            compScore+=1;
-        }else if(elements[choice]=="r"){
-            stat.innerText="You Won!";
-            yourScore+=1;
-        }else{
-            stat.innerText="Draw";
-        }
-    });
+    return elements[choice];
+};
+const check=(playersChoice)=>{
+    let ch=getCompSchoice();
+    if((playersChoice=="r" && ch=="p")||
+    (playersChoice=="p" && ch=="s")||
+    (playersChoice=="s" && ch=="r")
+    ){
+        stat.innerText="Computer Won!";
+        compScore+=1;
+    }else if(playersChoice==ch){
+        stat.innerText="Draw!";
+    }else{
+        stat.innerText="You won!";
+        yourScore+=1
+    }
     yourS.innerText=yourScore;
     compS.innerText=compScore;
 };
-check();
+rock.addEventListener("click",()=>{check("r")});
+paper.addEventListener("click",()=>{check("p")});
+scissor.addEventListener("click",()=>{check("s")});
+reset.addEventListener("click",()=>{
+    yourScore=0;
+    compScore=0;
+    yourS.innerText=0;
+    compS.innerText=0;
+    stat.innerText="Start playing!";
+});
 
